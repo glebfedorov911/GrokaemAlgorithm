d77d527f098790061134f1bf3701fad8a861f25c
glebfedorov911 <fedorov22134@gmail.com>
2025-04-30 20:49:46 +0300

    add steps for example

--- Thema7_Tree/bfs.py
+++ Thema7_Tree/bfs.py
@@ -4,15 +4,15 @@ import os
 
 def bfs(start_dir):
     dirs = deque()
-    dirs.append(start_dir)
+    dirs.append([start_dir, 0])
 
     while dirs:
-        dir = dirs.popleft()
+        dir, step = dirs.popleft()
         for file in sorted(os.listdir(dir)):
             fullpath = os.path.join(dir, file)
             if os.path.isfile(fullpath):
-                print(f"Найден файл с названием {file} по пути {fullpath}")
+                print(f"Найден файл с названием {file} по пути {fullpath}. Понадобилось пройти директорий: {step}")
             else:
-                dirs.append(fullpath)
+                dirs.append([fullpath, step+1])
 
 bfs("Thema7_Tree/test_catalog")
